3a61aead1abc202294aeb6feab2ddff65f8c1dca
Chris Hilsdon <chris@koolserve.uk>
2017-04-24 08:17:43 +0100

    Started working on user class

---
+++ src/User.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author Chris Hilsdon <chris@koolserve.uk>
+ */
+
+namespace Gitlab;
+
+class User extends Base
+{
+    public function create($email, $username, $name, $password, $config = [])
+    {
+        $url = '/api/v3/users/?email=' . $email;
+
+        $config['username'] = $username;
+        $config['name'] = $name;
+        $config['password'] = $password;
+
+        foreach ($config as $key => $value) {
+            $url .= '&' . $key . '=' . $value;
+        }
+
+        return $this->post($url);
+    }
+
+    public function fetch($id)
+    {
+        $url = '/api/v3/users/' . $id;
+        return $this->get($url);
+
+    }
+
+    public function fetchAll()
+    {
+        $url = '/api/v3/users';
+        return $this->get($url);
+    }
+}
